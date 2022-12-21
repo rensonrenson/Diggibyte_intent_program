a8f55b057b5e50c48fa03b373dbeec84b7f25d82
rensonrenson <30284753+rensonrenson@users.noreply.github.com>
2022-12-21 17:54:42 +0530

    Add files via upload

---
+++ calculate_age.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+import findspark
+from explode import spark
+findspark.init()
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DateType
+import findspark
+findspark.init()
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+
+df = spark.createDataFrame(
+    data=[
+        (1001, "arun",  datetime.strptime("1999-12-19", "%Y-%m-%d").date()),
+        (1002, "arul",  datetime.strptime("1989-12-14", "%Y-%m-%d").date()),
+    ],
+    schema=T.StructType([
+        T.StructField("id", T.IntegerType(), True),
+        T.StructField("name", T.StringType(), True),
+        T.StructField("birth_date", T.DateType(), True)
+    ])
+)
+df = df.withColumn("age_date", F.floor(F.datediff(F.current_date(), F.col("birth_date")) / 365.25))
+df.show()
